6c6cc6a676f9d34ff6c19add994427adf5b37087
Robert Smolčić <robert.smolcic@3mi-lab.hr>
2024-07-25 12:24:22 +0200

    #add cors

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Use cors middleware
+app.use(cors());
+
 app.use(express.json());
 
 app.post('/log', (req, res) => {
     const logMessage = req.body.logMessage;
     const logFilePath = path.join(__dirname, 'function_logs.txt');
+    console.log(logMessage);
     fs.appendFileSync(logFilePath, logMessage + '\n');
     res.sendStatus(200);
 });
