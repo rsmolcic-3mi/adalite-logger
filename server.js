d81df62ffaf24e1558bea3ef956303969d9a1f82
Robert Smolčić <robert.smolcic@3mi-lab.hr>
2024-07-25 16:18:57 +0200

    # update with uuid per request

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,9 +16,12 @@ app.use(express.json());
 app.post('/log', (req, res) => {
     const logMessage = req.body.logMessage;
     const logFilePath = path.join(__dirname, 'function_logs.txt');
+    const logId = uuidv4();
+    const logEntry = `GUID: ${logId}, Message: ${logMessage}`;
+
     console.log(logMessage);
-    fs.appendFileSync(logFilePath, logMessage + '\n');
-    res.sendStatus(200);
+    fs.appendFileSync(logFilePath, logEntry + '\n');
+    res.status(200).send(`Log entry created with GUID: ${logId}`);
 });
 
 app.listen(port, () => {
